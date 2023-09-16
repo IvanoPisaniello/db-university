916f874f94589f163bd2237c75ccd73f17b578b7
IvanoPisaniello <ivanop92@gmail.com>
2023-09-16 14:52:16 +0200

    added queries

---
+++ group_by.sql
@@ -0,0 +1,13 @@
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(`office_address`) AS 'quantity', `office_address` FROM `teachers` GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT ROUND(AVG(`vote`),2) AS 'vote_mid', `exam_id` FROM `exam_student` GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(*) AS 'department_number', `department_id` FROM `degrees` GROUP BY `department_id`;
